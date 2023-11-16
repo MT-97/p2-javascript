43ee3ce13a7d921ddd83c59f52cd41e9e12d8370
MT-97 <9007680@student.zadkine.nl>
2023-11-16 12:27:13 +0100

    w1

---
+++ app.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById("cookiewall");
+
+function showModal() {
+    modal.style.display = "block";
+}
+function removeModal() {
+    modal.style.display = "none";
+}
+showModal()
+    
